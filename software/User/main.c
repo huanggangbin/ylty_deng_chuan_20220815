0de7d22c7139e6f71489bb7e1e6f74f0e8a8c322
huanggangbin <136294447@qq.com>
2022-08-15 17:02:32 +0800

    首次上传

---
+++ software/User/main.c
@@ -0,0 +1,101 @@
+/*---------------------------------------------------------------------*/
+/* --- STC MCU Limited ------------------------------------------------*/
+/* --- STC 1T Series MCU Demo Programme -------------------------------*/
+/* --- Mobile: (86)13922805190 ----------------------------------------*/
+/* --- Fax: 86-0513-55012956,55012947,55012969 ------------------------*/
+/* --- Tel: 86-0513-55012928,55012929,55012966 ------------------------*/
+/* --- Web: www.STCMCU.com --------------------------------------------*/
+/* --- Web: www.STCMCUDATA.com  ---------------------------------------*/
+/* --- QQ:  800003751 -------------------------------------------------*/
+/* 如果要在程序中使用此代码,请在程序中注明使用了STC的资料及程序            */
+/*---------------------------------------------------------------------*/
+
+#include	"config.h"
+#include	"GPIO.h"
+#include	"delay.h"
+#include	"spi.h"
+/***************	功能说明	****************
+
+本例程基于STC8H8K64U为主控芯片的实验箱8进行编写测试，STC8G、STC8H系列芯片可通用参考.
+
+程序使用P6口来演示跑马灯，输出低驱动。
+
+下载时, 选择时钟 24MHz (可以在配置文件"config.h"中修改).
+
+******************************************/
+
+/*************	本地常量声明	**************/
+u8 code ledNum[]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+
+
+/*************	本地变量声明	**************/
+u8 ledIndex;
+
+
+/*************	本地函数声明	**************/
+
+
+/*************  外部函数和变量声明 *****************/
+
+
+/******************** IO配置函数 **************************/
+void	GPIO_config(void)
+{
+	GPIO_InitTypeDef	GPIO_InitStructure;		//结构定义
+	GPIO_InitStructure.Pin  = GPIO_Pin_2 | GPIO_Pin_3;		//指定要初始化的IO,
+	GPIO_InitStructure.Mode = GPIO_PullUp;		//指定IO的输入或输出方式,GPIO_PullUp,GPIO_HighZ,GPIO_OUT_OD,GPIO_OUT_PP
+	GPIO_Inilize(GPIO_P3,&GPIO_InitStructure);	//初始化
+	
+	
+
+	GPIO_InitStructure.Pin  = GPIO_Pin_4 | GPIO_Pin_5;		//指定要初始化的IO, GPIO_Pin_0 ~ GPIO_Pin_7
+	GPIO_InitStructure.Mode = GPIO_PullUp;		//指定IO的输入或输出方式,GPIO_PullUp,GPIO_HighZ,GPIO_OUT_OD,GPIO_OUT_PP
+	GPIO_Inilize(GPIO_P5,&GPIO_InitStructure);	//初始化
+}
+
+
+/****************  SPI初始化函数 *****************/
+void	SPI_config(void)
+{
+	SPI_InitTypeDef		SPI_InitStructure;
+	SPI_InitStructure.SPI_Module    = ENABLE;						//SPI启动    ENABLE, DISABLE
+	SPI_InitStructure.SPI_SSIG      = ENABLE;					//片选位     ENABLE, DISABLE
+	SPI_InitStructure.SPI_FirstBit  = SPI_MSB;					//移位方向   SPI_MSB, SPI_LSB
+	SPI_InitStructure.SPI_Mode      = SPI_Mode_Slave;	  //主从选择   SPI_Mode_Master, SPI_Mode_Slave
+	SPI_InitStructure.SPI_CPOL      = SPI_CPOL_Low;    //时钟相位   SPI_CPOL_High,   SPI_CPOL_Low
+	SPI_InitStructure.SPI_CPHA      = SPI_CPHA_2Edge;	  //数据边沿   SPI_CPHA_1Edge,  SPI_CPHA_2Edge
+	SPI_InitStructure.SPI_Interrupt = ENABLE;			  //中断允许   ENABLE,DISABLE
+	SPI_InitStructure.SPI_Speed     = SPI_Speed_4;		  //SPI速度    SPI_Speed_4, SPI_Speed_16, SPI_Speed_64, SPI_Speed_128
+	SPI_InitStructure.SPI_IoUse     = SPI_P22_P23_P24_P25; //IO口切换   SPI_P12_P13_P14_P15, SPI_P22_P23_P24_P25, SPI_P54_P40_P41_P43, SPI_P35_P34_P33_P32
+	SPI_Init(&SPI_InitStructure);
+	
+	SPI_SS_2 = 1;
+}
+
+
+/******************** 主函数 **************************/
+void main(void)
+{
+	GPIO_config();
+	SPI_config();
+
+	ledIndex = 0;
+	
+	while(1)
+	{
+		P5 = ~ledNum[ledIndex];	//输出低驱动
+		P3 = ~ledNum[ledIndex];	//输出低驱动
+		
+		ledIndex++;
+		if(ledIndex > 7)
+		{
+			ledIndex = 0;
+		}
+		delay_ms(250);
+		delay_ms(250);
+	}
+}
+
+
+
+
