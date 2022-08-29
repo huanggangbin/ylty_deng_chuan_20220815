ba1e324eac6322b84bf1e3b45ede4b3de20d7085
huanggangbin <136294447@qq.com>
2022-08-29 16:56:09 +0800

    1、增加串口通信，剩余外部中断

--- software/User/main.c
+++ software/User/main.c
@@ -15,6 +15,7 @@
 #include	"delay.h"
 #include	"spi.h"
 #include	"timer.h"
+#include	"UART.h"
 /***************	功能说明	****************
 
 本例程基于STC8H8K64U为主控芯片的实验箱8进行编写测试，STC8G、STC8H系列芯片可通用参考.
@@ -56,6 +57,9 @@ void	GPIO_config(void)
 	GPIO_InitStructure.Pin  = GPIO_Pin_5;		//指定要初始化的IO, GPIO_Pin_0 ~ GPIO_Pin_7
 	GPIO_InitStructure.Mode = GPIO_PullUp;		//指定IO的输入或输出方式,GPIO_PullUp,GPIO_HighZ,GPIO_OUT_OD,GPIO_OUT_PP
 	GPIO_Inilize(GPIO_P5,&GPIO_InitStructure);	//初始化
+	
+	
+	//LED, KEY, SPI, UART
 }
 
 
@@ -93,25 +97,52 @@ void	Timer_config(void)
 }
 
 
+/***************  串口初始化函数 *****************/
+void	UART_config(void)
+{
+	COMx_InitDefine		COMx_InitStructure;					//结构定义
+	COMx_InitStructure.UART_Mode      = UART_8bit_BRTx;	//模式, UART_ShiftRight,UART_8bit_BRTx,UART_9bit,UART_9bit_BRTx
+	COMx_InitStructure.UART_BRT_Use   = BRT_Timer1;			//选择波特率发生器, BRT_Timer1, BRT_Timer2 (注意: 串口2固定使用BRT_Timer2)
+	COMx_InitStructure.UART_BaudRate  = 115200ul;			//波特率, 一般 110 ~ 115200
+	COMx_InitStructure.UART_RxEnable  = ENABLE;				//接收允许,   ENABLE或DISABLE
+	COMx_InitStructure.BaudRateDouble = DISABLE;			//波特率加倍, ENABLE或DISABLE
+	COMx_InitStructure.UART_Interrupt = ENABLE;				//中断允许,   ENABLE或DISABLE
+	COMx_InitStructure.UART_Priority    = Priority_0;			//指定中断优先级(低到高) Priority_0,Priority_1,Priority_2,Priority_3
+	COMx_InitStructure.UART_P_SW      = UART1_SW_P54_P55;	//切换端口,   UART1_SW_P30_P31,UART1_SW_P36_P37,UART1_SW_P16_P17,UART1_SW_P43_P44
+	UART_Configuration(UART1, &COMx_InitStructure);		//初始化串口1 UART1,UART2,UART3,UART4
+
+	PrintString1("STC8G1K08A UART1 Test Programme!\r\n");	//UART1发送一个字符串
+}
+
+
 /******************** 主函数 **************************/
 void main(void)
 {
+	u8	i;
+	
 	GPIO_config();
 	//SPI_config();
-	Timer_config();
+	//Timer_config
+	UART_config();
 	EA = 1;
 
-	ledIndex = 0;
 	while(1)
 	{
 		//SPI_WriteByte(0x5a);
 		//SPDAT = 0x5a;
 		//delay_ms(1);
-		P5_status = P5;
-		ledIndex++;
-		if(ledIndex > 7)
+		//P5_status = P5;
+		delay_ms(1);
+		if(COM1.RX_TimeOut > 0)		//超时计数
 		{
-			ledIndex = 0;
+			if(--COM1.RX_TimeOut == 0)
+			{
+				if(COM1.RX_Cnt > 0)
+				{
+					for(i=0; i<COM1.RX_Cnt; i++)	TX1_write2buff(RX1_Buffer[i]);	//收到的数据原样返回
+				}
+				COM1.RX_Cnt = 0;
+			}
 		}
 		//delay_ms(25);
 		//delay_ms(25);
